330709fc5402efdc4bc1c275728a8639063393a2
陈远 <80950159@qq.com>
2023-12-14 18:11:55 +0800

    修复版2

---
+++ app/src/main/java/com/azeesoft/lib/colorpicker/simple/MainActivity.java
@@ -0,0 +1,30 @@
+package com.azeesoft.lib.colorpicker.simple;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.azeesoft.lib.colorpicker.ColorPickerDialog;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onTestClick(View view) {
+        ColorPickerDialog colorPickerDialog= ColorPickerDialog.createColorPickerDialog(
+                this,ColorPickerDialog.DARK_THEME);
+        colorPickerDialog.setOnColorPickedListener(new ColorPickerDialog.OnColorPickedListener() {
+            @Override
+            public void onColorPicked(int color, String hexVal) {
+                Toast.makeText(MainActivity.this,"color hex: "+hexVal,Toast.LENGTH_SHORT).show();
+            }
+        });
+        colorPickerDialog.setHexaDecimalTextColor(Color.WHITE);
+        colorPickerDialog.show();
+    }
+}
